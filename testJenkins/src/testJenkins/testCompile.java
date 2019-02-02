c1b8eb92d7b42b4a01cb03960a9854c3310df84d
vijaygovind <vijucg@yahoo.com>
2019-02-02 19:09:18 +0530

    test commit Signed-off-by: vijaygovind <vijucg@yahoo.com>

---
+++ testJenkins/src/testJenkins/testCompile.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package testJenkins;
+
+/**
+ * @author Vijay Govind
+ *
+ */
+public class testCompile {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Jai Vinayaka from compile");;
+
+	}
+
+}
