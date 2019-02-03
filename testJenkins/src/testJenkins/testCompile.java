676ccf2a25bf3e9c88a441bcf6bd080852f301f8
vijaygovind <vijucg@yahoo.com>
2019-02-03 17:19:22 +0530

    final test comment
    Signed-off-by: vijaygovind <vijucg@yahoo.com>

--- testJenkins/src/testJenkins/testCompile.java
+++ testJenkins/src/testJenkins/testCompile.java
@@ -15,7 +15,7 @@ public class testCompile {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Jai Vinayaka from compile");;
+		System.out.println("Jai Vinayaka from compile ----->>>");
 
 	}
 
